436ab7297347c9109c705103ecdd78def89fdbd0
julliermy <julliermy@192.168.0.7>
2025-04-26 19:20:16 -0300

    Remoção da instalação do VS Code

--- post-install-fedora.sh
+++ post-install-fedora.sh
@@ -8,18 +8,11 @@ sudo dnf install -y \
   https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
   https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-echo "Habilitando chave do vs code..."
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
-
 echo "Habilitando copr do Ani-Cli..."
 sudo dnf copr enable derisis13/ani-cli -y
 
 echo "Adicionando repositório Flatpak (Flathub)..."
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
-echo "Checando cache do pacote VS Code..."
-dnf check-update
 
 echo "Instalando pacotes via DNF..."
 sudo dnf install -y \
@@ -33,9 +26,8 @@ sudo dnf install -y \
   obs-studio \
   obs-studio-devel \
   obs-studio-libs \
-  https://github.com/th-ch/youtube-music/releases/download/v3.8.0/youtube-music-3.8.0.x86_64.rpm \
+  https://github.com/th-ch/youtube-music/releases/download/v3.8.1/youtube-music-3.8.1.x86_64.rpm \
   curl \
-  code \
   alsa-lib-devel \
   fftw3-devel \
   pulseaudio-libs-devel \
